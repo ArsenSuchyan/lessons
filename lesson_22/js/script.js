ec66f7a8946421e523492cf0a8cdc12414aa6982
ArsenSuchian <118843584+ArsenSuchyan@users.noreply.github.com>
2024-01-04 00:13:46 +0200

    Add files via upload

---
+++ lesson_22/js/script.js
@@ -0,0 +1,7 @@
+
+
+const icon = document.querySelector('.bottom-header__menu-button');
+
+icon.addEventListener ('click', function () {
+   document.documentElement.classList.toggle('menu-open');
+})
